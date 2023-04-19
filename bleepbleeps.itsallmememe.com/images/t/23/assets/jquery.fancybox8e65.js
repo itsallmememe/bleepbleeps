2bda6035aafd50f5fcbadb2995758b557f00a2f2
Niall McCormack <niall@itsallmememe.com>
2023-04-19 22:28:42 +0100

    all http to https

--- bleepbleeps.itsallmememe.com/images/t/23/assets/jquery.fancybox8e65.js
+++ bleepbleeps.itsallmememe.com/images/t/23/assets/jquery.fancybox8e65.js
@@ -3,7 +3,7 @@
  * version: 2.1.4 (Thu, 10 Jan 2013)
  * @requires jQuery v1.6 or later
  *
- * Examples at http://fancyapps.com/fancybox/
+ * Examples at https://fancyapps.com/fancybox/
  * License: www.fancyapps.com/fancybox/#license
  *
  * Copyright 2012 Janis Skarnelis - janis@fancyapps.com
